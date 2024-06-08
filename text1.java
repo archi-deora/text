53e11bd91874b3bc6e56e19f02981cc867f41b11
Archi <01fe22bca222@kletech.ac.in>
2024-06-08 19:15:25 +0530

    Print Hello World

---
+++ text1.java
@@ -0,0 +1,10 @@
+package text;
+
+public class text1 {
+    public class Hello {
+        public static void main(String[] args){
+            System.out.println("Hello World!");
+        }
+    }
+    
+}
